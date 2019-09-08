afb4f2b798ba5e8eab91c290b1760f4961b638b2
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-08 00:38:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,16 +21,31 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     student = {}
-    student[:profile_quote] = doc.css("div.profile-quote").text
-    student[:bio] = doc.css("div.bio-content p").text
-    doc.css("div.social-icon-controler a").each do |given_student|
-      url = given_student.attribute("href")
-      given_student[:github_url] = url if url.include?("github")
-      given_student[:linkedin_url] = url if url.include?("linkedin")
-      given_student[:blog_url] = url if given_student.css("img").attribute("src").text.include?("rss")
-      given_student[:twitter_url] = url if url.include?("twitter")
+  #   student[:profile_quote] = doc.css("div.profile-quote").text
+  #   student[:bio] = doc.css("div.bio-content p").text
+  #   doc.css("div.social-icon-controler a").each do |given_student|
+  #     url = given_student.attribute("href")
+  #     given_student[:github_url] = url if url.include?("github")
+  #     given_student[:linkedin_url] = url if url.include?("linkedin")
+  #     given_student[:blog_url] = url if given_student.css("img").attribute("src").text.include?("rss")
+  #     given_student[:twitter_url] = url if url.include?("twitter")
+  # end
+  container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
   end
-
   student
   end
 
