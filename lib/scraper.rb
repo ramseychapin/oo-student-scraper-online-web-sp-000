06df460b2c7c181a9d28c38b52a9f15d31a4d6e5
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-08 00:34:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,10 +23,10 @@ class Scraper
     student = {}
     doc.css("div.social-icon-controler a").each do |given_student|
       url = given_student.attribute("href")
-      given_student[:twitter_url] = url if url.include?("twitter")
-      given_student[:linkedin_url] = url if url.include?("linkedin")
       given_student[:github_url] = url if url.include?("github")
+      given_student[:linkedin_url] = url if url.include?("linkedin")
       given_student[:blog_url] = url if given_student.css("img").attribute("src").text.include?("rss")
+      given_student[:twitter_url] = url if url.include?("twitter")
   end
       student[:profile_quote] = doc.css("div.profile-quote").text
       student[:bio] = doc.css("div.bio-content p").text
